ce1bb8ec6b93bcc1d573c1739dec6977259ffab5
Ras-Kwesi <kwesi.makonnen@gmail.com>
2018-07-21 17:06:20 +0300

    background color and font styles added

--- js/scripts.js
+++ js/scripts.js
@@ -21,7 +21,6 @@ var Player2 = new Player()
 // User Interface Logic
 $(document).ready(function(){  // jquery to run after the html is fully ran
     $("#startbutton").click(function(event){ //jQuery to hold values submitted when button is clicked
-        alert("working");
 
         var playerOne = $("#player1name").val() // Reasign Player Names to new var
         var playerTwo = $("#player2name").val() 
